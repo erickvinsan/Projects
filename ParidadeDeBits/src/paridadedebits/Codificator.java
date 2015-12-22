6723e67f89a8273c1d4d38d821e1a162afd88f59
erickvinsan <erickvaladares@id.uff.br>
2015-12-22 11:35:07 -0200

    CodificadorOK

---
+++ ParidadeDeBits/src/paridadedebits/Codificator.java
@@ -0,0 +1,72 @@
+package paridadedebits;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+
+/**
+ *
+ * @author erick
+ */
+public class Codificator extends PairityBits {
+
+    private OutputStream fileWriter;
+
+    public Codificator(String fileName) throws FileNotFoundException{
+        super(fileName);
+    }
+
+    private byte[] getBlockOfBytes(List<Byte> bytes, int i) {//Pega blocos de 8 bytes.
+        byte[] bytesArray = new byte[8];
+        int blockId = i * 8;
+        for (int k = 0; k <  8; k++) {
+            bytesArray[k] = bytes.get(k + blockId);
+        }
+        return bytesArray;
+    }
+
+    public void makeCodification() throws IOException {
+        List<Byte> listOfBytes = makeArrayBytes();
+        System.out.println("Quantidade de bytes: " + (listOfBytes.size()));
+        fileWriter = new FileOutputStream("pacote.pck");
+        byte[] bytes;
+        System.out.println("");
+        Byte row;
+        Byte col;
+        int mult = listOfBytes.size() / 8;
+        System.out.println("Grupos de 8 bytes: " + mult + "\n");
+        byte[] finalBytes = new byte[10];
+        byte auxConst = 0;
+        
+        if ((listOfBytes.size() % 8) != 0) {//Número de bytes != múltiplo de 8. Preenche com 0's.
+            int rest = listOfBytes.size() - (mult * 8);
+            System.out.println("Resto = " + rest + "\n");
+            for (int i = 0; i < (8 - rest); i++) {//Preenche com 0's
+                listOfBytes.add(auxConst);
+            }
+            mult++;
+        }
+        
+        for (int i = 0; i < mult; i++) {// Codifica a Parte inteira da quantidade de bytes
+            bytes = getBlockOfBytes(listOfBytes, i);
+            System.out.println("Bloco = " + i);
+            for (int j = 0; j < 8; j++) {
+                System.out.print(" " + bytes[j]);
+            }
+            System.out.println("");
+            row = this.makeRowParity(bytes);
+            col = this.makeColParity(bytes);
+            finalBytes[0] = col;
+            finalBytes[1] = row;
+            System.arraycopy(bytes, 0, finalBytes, 2, 8);
+            fileWriter.write(finalBytes);
+            fileWriter.flush();
+        }
+        
+        fileWriter.close();
+    }
+
+    
+}
---
+++ ParidadeDeBits/src/paridadedebits/ParidadeDeBits.java
@@ -0,0 +1,46 @@
+package paridadedebits;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
+
+/**
+ *
+ * @author erick
+ */
+public class ParidadeDeBits {
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        System.out.println("Iniciando Codificacao...");
+        System.out.println("Lendo Arquivo...");
+        try (FileOutputStream f = new FileOutputStream("pacote.pck")) { 
+            Random r = new Random();
+            for (int i = 1; i <= 16; i++) {
+                f.write((byte)(i * Math.random()) );   
+                
+            }
+            f.flush();
+        }
+        
+        Codificator c = new Codificator("pacote.pck");
+        c.makeCodification();
+        //List<Byte> list = c.makeArrayBytes();
+        try (FileInputStream fin = new FileInputStream("pacote.pck")) {
+            //List<Byte> list = c.makeArrayBytes();
+            byte aux = 0;
+            System.out.println("\n"+ "Arquivo completo: ");
+            do {
+                aux = (byte) fin.read();
+                System.out.print(" " + aux);
+            } while (aux != -1);
+        }
+//
+//        String s = "01000001";
+//        int i = Integer.parseInt(s, 2);
+//        System.out.println((char) i);
+//        byte b = (byte) (Integer.parseInt(s) & 0xFF);
+//        System.out.println(b);
+    }
+}
