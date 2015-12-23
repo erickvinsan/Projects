dfac7030651a1cfa3f9075b00ad61995f5f1eaa0
erickvinsan <erickvaladares@id.uff.br>
2015-12-23 11:21:55 -0200

    "Decodificador1.19"

--- ParidadeDeBits/src/paridadedebits/ParidadeDeBits.java
+++ ParidadeDeBits/src/paridadedebits/ParidadeDeBits.java
@@ -15,22 +15,20 @@ public class ParidadeDeBits {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         System.out.println("Iniciando Codificacao...");
         System.out.println("Lendo Arquivo...");
-        try (FileOutputStream f = new FileOutputStream("pacote.pck")) { 
-            Random r = new Random();
+        try (FileOutputStream f = new FileOutputStream("pacote.pck")) {
             for (int i = 1; i <= 16; i++) {
-                f.write((byte)(i * Math.random()) );   
-                
+                f.write((byte) (i * Math.random()));
             }
             f.flush();
         }
-        
+
         Codificator c = new Codificator("pacote.pck");
         c.makeCodification();
         //List<Byte> list = c.makeArrayBytes();
         try (FileInputStream fin = new FileInputStream("pacote.pck")) {
             //List<Byte> list = c.makeArrayBytes();
             byte aux = 0;
-            System.out.println("\n"+ "Arquivo completo: ");
+            System.out.println("\n" + "Arquivo completo: ");
             do {
                 aux = (byte) fin.read();
                 System.out.print(" " + aux);
